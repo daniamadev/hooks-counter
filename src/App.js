ff64f4e113aa54fa4a735883a0ac18915abe988b
Daniela Aparecida Amadeu de Proença <amadev@amadev.localhost>
2020-06-25 11:36:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
 
-function App() {
+export default function App() {
+  const initialState = () => Number(window.localStorage.getItem("count")) || 0
+  const [count, setCount] = useState(initialState)
+
+  const incrementarCounter = () => setCount(count + 1)
+
+  const decrementarCounter = () => setCount(count - 1)
+
+  useEffect(() => {
+    window.localStorage.setItem("count", count)
+  }, [count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <h1>{count}</h1>
+      <button onClick={incrementarCounter}>Incrementar</button>
+      <button onClick={decrementarCounter}>Decrementar</button>
+    </React.Fragment>
+  )
 }
-
-export default App;
